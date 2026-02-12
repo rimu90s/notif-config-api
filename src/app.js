2bcf05299d7ba96bd5b38b91c4a211654ad443f7
rimu90s <ridwanmuchlis33@gmail.com>
2026-02-12 17:19:00 +0700

    initial project setup with Express foundation

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "API is running",
+  });
+});
+
+module.exports = app;
